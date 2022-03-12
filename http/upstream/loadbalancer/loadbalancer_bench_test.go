9de814d716e438a72d9ae203e7e035af6302488a
xgfone <xgfone@126.com>
2022-03-12 15:23:20 +0800

    fix: replace io.Discard

--- http/upstream/loadbalancer/loadbalancer_bench_test.go
+++ http/upstream/loadbalancer/loadbalancer_bench_test.go
@@ -16,7 +16,6 @@ package loadbalancer
 
 import (
 	"context"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,6 +25,10 @@ import (
 	"github.com/xgfone/go-apiserver/log"
 	"github.com/xgfone/go-apiserver/nets"
 )
+
+type discardWriter struct{}
+
+func (w discardWriter) Write(p []byte) (int, error) { return len(p), nil }
 
 type testServer struct{ url upstream.URL }
 
@@ -38,7 +41,7 @@ func (s testServer) HandleHTTP(http.ResponseWriter, *http.Request) error { retur
 func newTestServer(ip string) testServer { return testServer{url: upstream.URL{IP: ip}} }
 
 func BenchmarkLoadBalancer(b *testing.B) {
-	log.DefaultLogger = log.NewLogger(io.Discard, "", 0, log.LvlAlert)
+	log.DefaultLogger = log.NewLogger(discardWriter{}, "", 0, log.LvlAlert)
 	lb := NewLoadBalancer("test", balancer.Random())
 	lb.ResetServers(newTestServer("127.0.0.1"), newTestServer("127.0.0.2"))
 
