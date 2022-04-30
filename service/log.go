f0154e708698cd9c09b37c1ebcf6bdec264fedd2
xgfone <xgfone@126.com>
2022-04-30 09:44:56 +0800

    add the log service

---
+++ service/log.go
@@ -0,0 +1,39 @@
+// Copyright 2022 xgfone
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import "github.com/xgfone/go-apiserver/log"
+
+// LogService returns a new log Service, which will log the activated
+// or deactivated event.
+func LogService(logLevel int, serviceName string, service Service) Service {
+	return logService{service: service, level: logLevel, sname: serviceName}
+}
+
+type logService struct {
+	service Service
+	sname   string
+	level   int
+}
+
+func (s logService) Activate() {
+	log.Log(s.level, 0, "the service is activated", "service", s.sname)
+	s.service.Activate()
+}
+
+func (s logService) Deactivate() {
+	log.Log(s.level, 0, "the service is deactivated", "service", s.sname)
+	s.service.Deactivate()
+}
