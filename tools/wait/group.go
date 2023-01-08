98a008e0a4002788bda66c833f1a2fc540392847
xgfone <xgfone@126.com>
2023-01-08 22:43:44 +0800

    rename the files in the sub-package wait

--- tools/wait/group.go
+++ tools/wait/group.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package wait provides some tools to loop calling until a condition reaches.
 package wait
 
 import (
