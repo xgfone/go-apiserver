a5e3a8c32312ee40f737fe4ad10360c8fba54796
xgfone <xgfone@126.com>
2023-02-23 21:16:02 +0800

    add the log switch handler

---
+++ log/handler.go
@@ -0,0 +1,70 @@
+// Copyright 2023 xgfone
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package log
+
+import (
+	"context"
+	"sync/atomic"
+
+	"golang.org/x/exp/slog"
+)
+
+type handlerWrapper struct{ Handler slog.Handler }
+
+// SwitchHandler is a log handler to switch the handler.
+type SwitchHandler struct {
+	handler atomic.Value
+}
+
+// NewSwitchHandler returns a new slog handler.
+func NewSwitchHandler(handler slog.Handler) *SwitchHandler {
+	if handler == nil {
+		panic("SwitchHandler: slog handler is nil")
+	}
+
+	sh := new(SwitchHandler)
+	sh.handler.Store(handlerWrapper{handler})
+	return sh
+}
+
+// Swap swaps the old handler with the new.
+func (h *SwitchHandler) Swap(new slog.Handler) (old slog.Handler) {
+	return h.handler.Swap(handlerWrapper{new}).(handlerWrapper).Handler
+}
+
+// Get returns the wrapped handler.
+func (h *SwitchHandler) Get() slog.Handler {
+	return h.handler.Load().(handlerWrapper).Handler
+}
+
+// Enabled implements the interface slog.Handler#Enabled.
+func (h *SwitchHandler) Enabled(c context.Context, l Level) bool {
+	return h.Get().Enabled(c, l)
+}
+
+// Handle implements the interface slog.Handler#Handle.
+func (h *SwitchHandler) Handle(r slog.Record) error {
+	return h.Get().Handle(r)
+}
+
+// WithAttrs implements the interface slog.Handler#WithAttrs.
+func (h *SwitchHandler) WithAttrs(attrs []Attr) slog.Handler {
+	return h.Get().WithAttrs(attrs)
+}
+
+// WithGroup implements the interface slog.Handler#WithGroup.
+func (h *SwitchHandler) WithGroup(name string) slog.Handler {
+	return h.Get().WithGroup(name)
+}
