fb5f4a6370aee4c109d840a9877f300005b9646a
xgfone <xgfone@126.com>
2023-04-05 15:58:46 +0800

    fix an error

--- log/log.go
+++ log/log.go
@@ -25,7 +25,7 @@ import (
 	"time"
 
 	"github.com/xgfone/go-apiserver/helper"
-	"github.com/xgfone/go-apiserver/tools/io2"
+	"github.com/xgfone/go-apiserver/io2"
 	"github.com/xgfone/go-atexit"
 	"github.com/xgfone/go-defaults"
 	"golang.org/x/exp/slog"
